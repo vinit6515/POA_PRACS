0bd166658533f083cd00dba9451a7dc494e5dace
vinit6515 <143284532+vinit6515@users.noreply.github.com>
2024-05-16 12:14:21 +0530

    16 bit addition

---
+++ addition16-bit.asm
@@ -0,0 +1,21 @@
+data segment 
+    num1 db 0005h;
+    num2 db 0004h;
+    res db dup(?);
+    data ends
+code segment 
+    assume cs: code segment ds: data
+        start:
+        mov ax,data;
+        mov ds,ax;
+        lea SI,num1;
+        mov ax,[SI];
+        lea SI , num2;
+        mov bx , [SI];
+        add ax ,bx
+        lea SI, res;
+        mov [SI],ax;
+        hlt
+        code ends
+    end start
+    
